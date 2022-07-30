06e448785a7859954bc4ba217955a7982743132b
qiaofeng1227 <76487013@qq.com>
2022-07-30 09:45:27 +0800

    Update get_version.sh

--- src/get_version.sh
+++ src/get_version.sh
@@ -1,1 +1,1 @@
-sudo echo "oracle version:" $(cat /data/apps/oracle/.env |grep APP_VERSION|cut -d= -f2)  1>> /data/logs/install_version.txt
+sudo echo "oracle version:" $(docker exec -i $1 ls /opt/oracle/product)  1>> /data/logs/install_version.txt
